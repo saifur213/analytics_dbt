c05bc37fc441446691d1c9a3f48d282c6a9217e7
saifur213 <ksaifur13@gmail.com>
2026-04-26 17:40:41 +0600

    Fix CI/CD

--- models/orders_stg.sql
+++ models/orders_stg.sql
@@ -7,29 +7,32 @@
 }}
 
 SELECT
-    ORDERID,
-    ORDERDATE,
-    CUSTOMERID,
-    EMPLOYEEID,
-    STOREID,
-    STATUS AS STATUSCD,
-
-    UPDATED_AT,
-
-    CASE
-        WHEN STATUS = '01' THEN 'In Progress'
-        WHEN STATUS = '02' THEN 'Completed'
-        WHEN STATUS = '03' THEN 'Cancelled'
-    END AS STATUSDESC,
+    src.orderid,
+    src.orderdate,
+    src.customerid,
+    src.employeeid,
+    src.storeid,
+    src.status AS statuscd,
+    src.updated_at,
 
     CASE
-        WHEN STOREID = 1000 THEN 'Online'
+        WHEN src.status = '01' THEN 'In Progress'
+        WHEN src.status = '02' THEN 'Completed'
+        WHEN src.status = '03' THEN 'Cancelled'
+    END AS statusdesc,
+
+    CASE
+        WHEN src.storeid = 1000 THEN 'Online'
         ELSE 'In-store'
-    END AS ORDER_CHANNEL,
-    current_timestamp AS DBT_UPDATED_AT
+    END AS order_channel,
 
-FROM {{ source('revenue_dev', 'orders') }}
+    CURRENT_TIMESTAMP AS dbt_updated_at
+
+FROM {{ source('revenue_dev', 'orders') }} AS src
 
 {% if is_incremental() %}
-    WHERE UPDATED_AT >= (SELECT max(UPDATED_AT) FROM {{ this }})
+WHERE src.UPDATED_AT >= (
+    SELECT MAX(tgt.UPDATED_AT)
+    FROM {{ this }} AS tgt
+)
 {% endif %}
